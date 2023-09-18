4a4c385688baa64bb44a0a98ddaa0629787ae697
PaolaMoriconi <paomori18@gmail.com>
2023-09-18 10:36:33 -0300

    calculadora

---
+++ app.js
@@ -0,0 +1,29 @@
+//todo: hacer el front de la calculadora
+
+const process = require('process')
+const {argv} = process;
+function calculadora (comando) {
+    
+
+
+switch (comando) {
+    case "sumar":
+        
+        break;
+        case "restar":
+        
+        break;
+        case "multiplicar":
+        
+        break;
+        case "dividir":
+        
+        break;
+
+
+    default:
+        return "comando no valido"
+        break;
+}
+}
+console.log(calculadora(argv[2]));
